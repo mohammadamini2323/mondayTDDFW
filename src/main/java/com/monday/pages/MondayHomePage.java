1bd71878cb7f5cacd150d32c0af69b27be6589a6
mohammadamini2323 <moh.amini2323@gmail.com>
2025-03-09 17:15:50 -0400

    Update MondayHomePage.java

--- src/main/java/com/monday/pages/MondayHomePage.java
+++ src/main/java/com/monday/pages/MondayHomePage.java
@@ -10,6 +10,32 @@ public class MondayHomePage {
     WebDriver driver;
     @FindBy(xpath ="span.PHBU1")
     WebElement mondayText;
+    @FindBy(xpath ="//div[@data-testid='topbar-notifications']")
+    WebElement notificationIcon;
+      @FindBy(xpath ="//div[@data-testid='topbar-inbox']")
+    WebElement iboxIcon;
+      @FindBy(xpath ="//div[@data-testid='topbar-invite-members']")
+    WebElement iviteMemberIcon;
+      @FindBy(xpath ="//div[@data-testid='topbar-apps']")
+    WebElement puzzelAppsIcon;
+      @FindBy(xpath ="//div[@data-testid='topbar-search-everything']")
+    WebElement searchIcon;
+    ////button[@aria-label='Search everything']
+      @FindBy(xpath ="//button[@aria-label='Search everything']")
+    WebElement searchIcon;
+     ////span[@class='referenceWrapper_08111980c8']/child::button[@aria-label='Close']
+     @FindBy(xpath ="//span[@class='referenceWrapper_08111980c8']/child::button[@aria-label='Close']")
+    WebElement closeNotificationAlert;
+    ////button[@aria-label='Help']
+      @FindBy(xpath ="//button[@aria-label='Help']")
+    WebElement helpIcon;
+      @FindBy(xpath ="//button[@aria-label='Products switcher']")
+    WebElement gridIcon;
+      @FindBy(xpath ="//img[@alt='Mohammad Amini']")
+    WebElement imgIcon;
+      @FindBy(xpath ="//a[@aria-label='Home']")
+    WebElement homePageLink;
+    
     public MondayHomePage(WebDriver driver){
         this.driver=driver;
     }
